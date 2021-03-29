c2b302d01fbf1fe3422b62596abe600876cd0645
Nikolai Aristov <KodiStudios@gmail.com>
2021-03-28 19:06:50 -0700

    Add Test

---
+++ OneNoteMidiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OneNoteMidiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OneNoteMidiTest")]
+[assembly: AssemblyCopyright("Copyright © Kodi Studios 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ed1243e-12dc-4482-9b54-5fa63c32fc1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
